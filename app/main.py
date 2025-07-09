483f05fddcc4823555f7e2b75d51445e740705e1
Med-Amine-03 <medamineothmani03@gmail.com>
2025-07-09 18:18:34 +0100

    first commit

--- app/main.py
+++ app/main.py
@@ -4,12 +4,21 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import os
+from fastapi.middleware.cors import CORSMiddleware
 
 
 model_path = os.path.join(os.path.dirname(__file__), "model", "titanic_pipeline.pkl")
 model = joblib.load(model_path)
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # ou ["*"] si tu veux autoriser toutes origines (moins sécurisé)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Passenger(BaseModel):
     Pclass: int
